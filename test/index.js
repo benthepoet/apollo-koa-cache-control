ca3238c6de73738864b30f662e9fa867f13f50d2
Ben Hanna <benpaulhanna@gmail.com>
2018-04-25 21:40:20 +0000

    Added a unit test.

--- test/index.js
+++ test/index.js
@@ -1,5 +1,29 @@
 const assert = require('assert');
+const _ = require('underscore');
+
+const { cacher, getMinAge } = require('../');
 
 describe('Cache Control', () => {
+
+  const generateHints = _.compose(
+    _.partial(_.map, _, maxAge => ({ maxAge })),
+    _.shuffle,
+    _.range
+  );
+
+  describe('getMinAge', () => {
+    const body = {
+      extensions: {
+        cacheControl: {
+          hints: generateHints(50)
+        }
+      }
+    };
+    
+    it('should return lowest age', () => {
+      const minAge = getMinAge(body);
+      assert.equal(minAge, 0, '\'minAge\' was not 0.');
+    });
+  });
 
 });
